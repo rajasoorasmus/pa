3c1422c88fde8dca4f70c24912b81171eaca33e9
rasmus.rajasoo <rasmus.rajasoo@khk.ee>
2019-09-25 11:37:54 +0300

    Defineerimine ja kasutamine

---
+++ 25.09.2019/input.php
@@ -0,0 +1,7 @@
+<form action=""></form>
+<div>
+    <label for="nimi"></label>
+    <input type="text" id="Eesnimi" name="name">
+</div>
+    <button type="submit">Saada</button>
+
---
+++ 25.09.2019/input_do.php
@@ -0,0 +1,6 @@
+<?php
+$Eesnimi = $_GET['Eesnimi'];
+
+echo 'Nimi: '.$Eesnimi. '<div>';
+echo '<div>';
+echo '<a href="input.php">Sisesta andmed uuesti</a>';
