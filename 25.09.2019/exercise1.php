25372f4c7e75af00a57ec500b8e1023555e03be2
rasmus.rajasoo <rasmus.rajasoo@khk.ee>
2019-09-25 11:06:52 +0300

    Defineerimine ja kasutamine

--- 25.09.2019/exercise1.php
+++ 25.09.2019/exercise1.php
@@ -3,4 +3,4 @@
  Info
  */
 
-echo "Eesnimi: Rasmus<br>Perenimi: Rajasoo<br>Email: rasmusrajasoo@hotmail.com<br>Kooli email: rasmus.rajasoo@khk.ee<br>Vanus: 20<br>Pikkus: 185cm/6'1<br>Kaal: 92 KG";
+echo "Eesnimi: Rasmus<br>Perenimi: Rajasoo<br>Email: rasmusrajasoo@hotmail.com<br>Kooli email: rasmus.rajasoo@khk.ee<br>Vanus: 20<br>Pikkus: 185cm/6'1/1.85m<br>Kaal: 92 KG";
---
+++ 25.09.2019/variables.php
@@ -0,0 +1,24 @@
+<?php
+// Muutuja defineerimine
+// $muutujaNimi = väärtus;
+
+// Väärtused - andmed - andmetüübid
+// Tekst - string - "info jutumärkides"
+// Täisarvud - integer - 2, 7, 9, -15
+// Komaga arvud - float - 3.5, 58.56, 2.53
+
+$Eesnimi = "Rasmus";
+$Perenimi = "Rajasoo";
+$Email = "rasmusrajasoo@hotmail.com";
+$KhkEmail = "rasmus.rajasoo@hotmail.com";
+$Vanus = 20;
+$Pikkus = 1.85;
+$Kaal = 92;
+
+echo "Eesnimi: " . $Eesnimi. "<br>";
+echo "Perenimi: " . $Perenimi. "<br>";
+echo "Email: " . $Email. "<br>";
+echo "KhkEmail: " . $KhkEmail. "<br>";
+echo "Vanus: " . $Vanus. "<br>";
+echo "Pikkus: " . $Pikkus."m". "<br>";
+echo "Kaal: " . $Kaal."kg". "<br>";
