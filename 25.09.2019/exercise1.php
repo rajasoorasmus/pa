89284ec2c49bea6fb200a2375a00ede16686a3e9
rasmus.rajasoo <rasmus.rajasoo@khk.ee>
2019-09-25 10:36:52 +0300

    Minu enda andmed

---
+++ 25.09.2019/exercise1.php
@@ -0,0 +1,6 @@
+<?php
+/*
+ Info
+ */
+
+echo "Eesnimi: Rasmus<br>Perenimi: Rajasoo<br>Email: rasmusrajasoo@hotmail.com<br>Kooli email: rasmus.rajasoo@khk.ee<br>Vanus: 20<br>Pikkus: 185cm/6'1<br>Kaal: 92 KG";
